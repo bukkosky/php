2099a1b1a8dfc539e866e59ff4f6996c3d5adb05
Marcin_ch <bukkosky@gmail.com>
2019-02-18 21:37:19 +0100

    login with db connetion

---
+++ login/index.php
@@ -0,0 +1,26 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Login Form</title>
+</head>
+<body>
+
+	<form action="include/signup.inc.php" method="POST">
+		<input type="text", name="first", placeholder="First Name">
+		<br>
+		<input type="text", name="last", placeholder="Last Name">
+		<br>
+		<input type="email", name="email" placeholder="E-mail">
+		<br>
+		<input type="text", name="uid" placeholder="Username">
+		<br>
+		<input type="password", name="pwd" placeholder="Password">
+		<br>
+		<input type="submit">
+	</form>
+
+</body>
+</html>
